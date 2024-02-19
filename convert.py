c472ca89fdc2bc1f6c8d1d92deeae962c99b472e
eunseolim <eunseolim1018@gmail.com>
2024-02-19 19:11:38 +0900

    aac to mp3

--- convert.py
+++ convert.py
@@ -4,10 +4,10 @@ from fastapi import UploadFile
 
 def aac_to_mp3(file: UploadFile, file_name, export_path):
     # 업로드된 파일을 저장
-    basic_path = os.path.dirname(os.path.abspath)
+    basic_path = os.path.dirname(os.path.abspath(__file__))
     upload_file_path = os.path.join(basic_path, file.filename)
-    with open(upload_file_path, "w") as f:
-        f.write(file.read())
+    with open(upload_file_path, "wb") as f:
+        f.write(file.file.read())            
     
     # acc파일을 AudioSegment 객체 이용해서 mp3파일로 변환
     audio = AudioSegment.from_file(upload_file_path, format="aac")
