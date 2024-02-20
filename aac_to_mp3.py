88740c8c903714d64701b0537b0d9eb18549e1d0
eunseolim <eunseolim1018@gmail.com>
2024-02-20 16:04:45 +0900

    aac파일을 mp3파일로 변환

---
+++ aac_to_mp3.py
@@ -0,0 +1,14 @@
+from pydub import AudioSegment
+import os
+from fastapi import UploadFile
+
+def aac_to_mp3(file: UploadFile, file_name, export_path):
+    # 업로드된 파일을 저장
+    basic_path = os.path.dirname(os.path.abspath(__file__))
+    upload_file_path = os.path.join(basic_path, file.filename)
+    with open(upload_file_path, "wb") as f:
+        f.write(file.file.read())            
+    
+    # acc파일을 AudioSegment 객체 이용해서 mp3파일로 변환
+    audio = AudioSegment.from_file(upload_file_path, format="aac")
+    audio.export("{}/{}.mp3".format(export_path, file_name), format="mp3")
